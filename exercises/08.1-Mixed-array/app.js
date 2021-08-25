b8a1dbdc9cd9787d86be8897cd3a802abe711906
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-08-25 02:50:22 +0000

    Respaldo

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,9 @@
 var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 //your code here
+var newArray = [];
+for(let i = 0; i <= newArray.length; i--){
+    typeof[i].push("newArray");
+}
+
+console.log(newArray);
